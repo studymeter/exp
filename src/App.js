11d5721eeb9d95999b24ba08f41310dec1efaa67
Asahi300 <asahi.minowa@gmail.com>
2022-11-16 21:13:51 +0900

    SNSシェアボタン

--- src/App.js
+++ src/App.js
@@ -801,27 +801,39 @@ function App() {
                             <p>
                               <small>発行者</small><br />
                               {selectedNft.ca_name}
-                              {/* <OverlayTrigger
-                                key="copy"
-                                placement="top"
-                                overlay={
-                                  <Tooltip>コピー</Tooltip>
-                                }
-                              >
-                                <Button className="mt-1" variant="outline-dark" size="sm" onClick={()=>{navigator.clipboard.writeText(selectedNft.token_address);}}>
-                                  {selectedNft.token_address.substring(0,4)}...{selectedNft.token_address.substring(38)}
-                                  <span className="ms-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-clipboard" viewBox="0 0 16 16">
-                                      <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
-                                      <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
-                                    </svg>
-                                  </span>
-                                </Button>
-                              </OverlayTrigger> */}
                             </p>
                           </Col>
                         </Row>
-                        {/* <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a> */}
+                        <p>
+                          <small>発行者コントラクトアドレス</small>
+                          <OverlayTrigger
+                            key="copy"
+                            placement="top"
+                            overlay={
+                              <Tooltip>コピー</Tooltip>
+                            }
+                          >
+                            <Button className="mt-1 ps-0" variant="text" size="sm" onClick={()=>{navigator.clipboard.writeText(selectedNft.token_address);}}>
+                              {selectedNft.token_address}
+                              <span className="ms-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-clipboard" viewBox="0 0 16 16">
+                                  <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
+                                  <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
+                                </svg>
+                              </span>
+                            </Button>
+                          </OverlayTrigger>
+                        </p>
+                        <Button variant="text" href={"https://twitter.com/intent/tweet?hashtags=QWeb3,証明トークン&text=" + selectedNft.name + "の証明書を受け取りました！" + selectedNft.image} target="_blank" rel="noreferrer">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+                            <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+                          </svg>
+                        </Button>
+                        <Button variant="text" href={"https://www.facebook.com/sharer/sharer.php?u=" + selectedNft.image} target="_blank" rel="noreferrer">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                            <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+                          </svg>
+                        </Button>
                         {/* <hr />
                         <p className="text-center mt-3 mb-0">
                           <Button className="text-danger" variant="link">
